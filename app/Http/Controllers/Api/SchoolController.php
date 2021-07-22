a6694a907fbde6ed9a84a5920a06cbd0d80ce40e
oguni <orkney092@gmail.com>
2021-07-22 15:57:58 +0900

    [update]Schoolコントローラーの作成

--- app/Http/Controllers/Api/SchoolController.php
+++ app/Http/Controllers/Api/SchoolController.php
@@ -3,9 +3,22 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
-use Illuminate\Http\Request;
+use App\Models\School;
+use PHPUnit\TextUI\XmlConfiguration\CodeCoverage\FilterMapper;
 
 class SchoolController extends Controller
 {
-    //
+  /**
+   * Display a listing of the resource.
+   *
+   * @return \Illuminate\Http\Response
+   */
+  public function index()
+  {
+    $schools = School::all()->toArray();
+    $schools = array_map(function ($school) {
+      return $school['school_name'];
+    }, $schools);
+    return response()->json($schools);
+  }
 }
